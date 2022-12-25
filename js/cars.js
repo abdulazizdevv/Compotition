6d4e8495381f6bd706f43543de0533097192a332
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2022-12-25 12:50:02 +0500

    first commit

---
+++ js/cars.js
@@ -0,0 +1,114 @@
+let cars = [
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+  {
+    name: "Ford Fiesta",
+    type: "Economy Car",
+    img: Object.assign(new Image(), {
+      src: "http://i.stack.imgur.com/KUvWS.jpg",
+    }),
+  },
+];
