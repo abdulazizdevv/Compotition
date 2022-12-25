24b60503e67ef44d6c61c50e1800ca71aae35b90
MuhammadqodirProgrammer <komilovm831@gmail.com>
2022-12-25 20:35:35 +0500

    last commit

--- js/corusel.js
+++ js/corusel.js
@@ -6,9 +6,28 @@ $('.corusel_sliks-container').slick({
     dots: true,
   });
 
-  // $('.autoplay').slick({
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   autoplay: true,
-  //   autoplaySpeed: 2000,
-  // });
+ 
+  $('.community-corusels').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: '20px',
+    variableWidth: true
+  });
+
+  $('.project__corusel_toleft').slick({
+    autoplay:true,
+    infinite:true,
+    autoplaySpeed:0,
+    slidesToScroll:1,
+    slidesToShow:5,
+    arrows:false,
+    cssEase:'linear',
+    speed:1000,
+    initialSlide:1,
+    draggable:false,
+    //  rtl:true,
+  });
+ 
