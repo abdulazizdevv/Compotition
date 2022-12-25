abd83c056c24e86cba639e38c2e72a845e7ca13f
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2022-12-25 14:56:34 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ let elList = document.querySelector(".carsList");
 let elAllBtns = document.querySelector(".js-btns");
 let elCompact = document.querySelector(".js-compact");
 let elVans = document.querySelector(".js-vans");
+let elView = document.querySelector(".view");
 
+let newS = [];
 function renderCars(cars) {
   let res = "";
   for (i of cars) {
@@ -12,27 +14,27 @@ function renderCars(cars) {
         <h3 class="card-title">${i.name}</h3>
         <h6 style="" class="card-text">${i.type}</h6>
     </div>
-    <img src="${i.img}" class="card-img-top" alt="pokemons">
+    <img src="${i.img}" class="card-img-top" alt="car">
 
     <div style="display: flex; justify-content: space-between;">
         <div style="display: flex;">
-            <img style="margin-left: 27px;" src="${i.imgSite}" class="card-img-top" alt="pokemons">
+            <img style="margin-left: 27px;" src="${i.imgSite}" class="card-img" alt="car">
             <p style="margin-left: 14px;">5 Seats</p>
         </div>
-        <div style="display: flex;margin-right: 27px;">
-            <img src="${i.imgAuto}" class="card-img-top" alt="pokemons">
-            <p style="margin-left: 14px;">Automatic</p>
+        <div style="display: flex;margin-right: 89px; ">
+            <img src="${i.imgAuto}" class="card-img" alt="car">
+            <p style="margin-left: 12px; margin-right:8px;">Automatic</p>
         </div>
     </div>
 
     <div style="display: flex; justify-content: space-between;margin-top:24px;">
         <div style="display: flex;">
-            <img style="margin-left: 27px;" src="${i.imgYears}" class="card-img-top" alt="pokemons">
+            <img style="margin-left: 27px;" src="${i.imgYears}" class="card-img" alt="car">
             <p style="margin-left: 14px;">21+ Years</p>
         </div>
-        <div style="display: flex;margin-right: 27px;">
-            <img src="${i.imgOil}" class="card-img-top" alt="pokemons">
-            <p style="margin-left: 14px;">1-lit / 2.5 km</p>
+        <div style="display: flex;margin-right: 89px; ">
+            <img src="${i.imgOil}" class="card-img" alt="car">
+            <p style="margin-left: 14px; ">1-lit / 2.5 km</p>
         </div>
     </div>
 </div>
@@ -40,6 +42,17 @@ function renderCars(cars) {
   }
 
   elList.innerHTML = res;
+
   return res;
 }
-renderCars(cars);
+
+
+
+elView.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  renderCars(cars);
+});
+
+console.log(cars);
+let newsplice = cars.slice(0, 6);
+renderCars(newsplice);
